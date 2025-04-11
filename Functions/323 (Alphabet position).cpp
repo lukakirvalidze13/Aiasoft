4e246921cb7ce7fcb273617fa00795fd77c13561
SALVATORE <lukayt1996@gmail.com>
2025-04-11 12:51:02 +0400

    aia

---
+++ Functions/323 (Alphabet position).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int getSymbolPosition(char letter){
+    return ( letter - 'a') + 1;
+}
+int main(){
+
+    char letter;
+    cin >> letter;
+    int position = getSymbolPosition(letter);
+    cout << position;
+
+
+    return 0;
+}
