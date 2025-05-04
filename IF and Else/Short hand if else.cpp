ea2228246c9de3f1663de5dd9896f706c3365ee5
SALVATORE <lukayt1996@gmail.com>
2025-05-05 03:17:39 +0400

    new

---
+++ IF and Else/Short hand if else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int time;
+    cin >> time;
+    string result = (time < 12) ? "Good morning" : "Good evening";
+    cout << result;
+
+
+
+
+
+
+
+    return 0;
+}
